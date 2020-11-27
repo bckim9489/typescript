8b85fef3be02961e1fb830f559c303770a4f9948
bckim9489 <47053587+bckim9489@users.noreply.github.com>
2020-11-27 16:49:34 +0900

    EcmaScript

--- ecmascript/ecmascriptStd.js
+++ ecmascript/ecmascriptStd.js
@@ -1,31 +1,31 @@
 /*
  * 
  */
-const ln = args =>{if(!args){console.log(`-------------------------`)} else {console.log(`------------${args}-------------`)}};
+const ln = args => { if (!args) { console.log(`-------------------------`) } else { console.log(`------------${args}-------------`) } };
 
 //[ECMAScript] 나머지 연산자 Rest Operator
-const [a, ...restArr] = [1,2,3,4,5,6,7];
+const [a, ...restArr] = [1, 2, 3, 4, 5, 6, 7];
 ln();
 //[ECMAScript] 전개연산자
-function logThing(a, b, c){
+function logThing(a, b, c) {
     console.log(a);
     console.log(b);
     console.log(c);
 }
-const arr = [1,2,3];
+const arr = [1, 2, 3];
 logThing(...arr);
-logThing(...[1,2,3]);
+logThing(...[1, 2, 3]);
 
-const {x, y,...z}= {
-    x:1,
-    y:2,
-    a:3,
-    b:4
+const { x, y, ...z } = {
+    x: 1,
+    y: 2,
+    a: 3,
+    b: 4
 };
 x; // 1
 y; // 2
 z; // {a:3, b:4};
-const n = { x, y, ...z};
+const n = { x, y, ...z };
 n; // { x:1, y:2, {a:3, b:4}}
 ln();
 //[ECMAScript2015] Trailing Comma - 객체리터럴
@@ -35,13 +35,13 @@ var obj = {
     age: 42,
 };
 //[ECMAScript2017] Trailing Comma - 매개변수 목록
-function poo(p,){};
+function poo(p,) { };
 //Lambda도 가능
-(p,) => {};
+(p,) => { };
 //class 가능
 class C {
-    one(a,){}//,
-    two(a,b, ){}//,
+    one(a,) { }//,
+    two(a, b,) { }//,
 }
 //Rest 에는 불가능
 //function f(...p,){}
@@ -58,39 +58,39 @@ const objWithFunction = {
     f: function () {
         console.log(1);
     },
-    f: function() {
+    f: function () {
 
     }
 }
 //new (ES6~)
 const objWithFunction2 = {
-    f(){
+    f() {
         console.log(1);
     }
 }
 ln();
 //[ES6]계산된 속성이름
 const name = 'Bernard';
-const obj1 = {[name]: 'Werber'};
+const obj1 = { [name]: 'Werber' };
 console.log(obj); //{Bernard: 'Werber'}
-const obj3 = {['ab'+'c']:3};
+const obj3 = { ['ab' + 'c']: 3 };
 console.log(obj3) //{abc: 3}
 ln();
 //[ES6]기본 매개변수 지원
-function userDefault(a=1,b=1,c=1){
-    console.log([a,b,c]);
+function userDefault(a = 1, b = 1, c = 1) {
+    console.log([a, b, c]);
 }
-userDefault(0,0);
+userDefault(0, 0);
 ln();
 //[ES6]화살표 함수
 console.log("----------------");
 const obj2 = {
     a: 1,
-    normalFunc: function() { console.log(this); },
+    normalFunc: function () { console.log(this); },
     arrowFunc: () => { console.log(this); },
 };
 const { normalFunc, arrowFunc } = obj2;
-obj2.normalFunc(); 
+obj2.normalFunc();
 /* 
 { 
    a: 1,
@@ -98,15 +98,15 @@ obj2.normalFunc();
    arrowFunc: [Function: arrowFunc] 
 }
 */
-normalFunc(); 
+normalFunc();
 // undefined
 obj2.arrowFunc(); // (global object)
 arrowFunc(); // (global object)
 //인자가 한개이면 괄호 생략가능
-const vx = args => {console.log(args)};
+const vx = args => { console.log(args) };
 console.log(vx);
 //한줄 표현식이면 대괄호 생략 가능
-const isOdd = n => (n%2===1);
+const isOdd = n => (n % 2 === 1);
 console.log(isOdd(3));
 ln();
 //템플릿 리터럴
@@ -140,8 +140,8 @@ langs.forEach(lang => {
  * 이터러블(iterable) 프토로콜을 구현한(순회가능한) 객체에서만 됨
  */
 const languagues = ['javaScript', 'TypeScript', 'rust'];
-for(const elem of languagues){
-    if(!elem.includes('Script')){
+for (const elem of languagues) {
+    if (!elem.includes('Script')) {
         break;
     }
     console.log(elem);
@@ -154,15 +154,15 @@ ln();
  * 호출 될 때마다 아래 두 가지 값을 담은 객체를 반환해야 한다.
  */
 //이터레이터 예
-function makeIterator(array){
+function makeIterator(array) {
     var nextIndex = 0;
     return {
         next: () => {
-            return nextIndex < array.length ? {value : array[nextIndex++], done : false}: {done : true};
+            return nextIndex < array.length ? { value: array[nextIndex++], done: false } : { done: true };
         }
     };
 }
-const iter = makeIterator([1,2,3]);
+const iter = makeIterator([1, 2, 3]);
 console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
@@ -174,32 +174,32 @@ ln();
  * 해당 메소드를 실행했을 때 이터레이터 인스턴스가 반환될 때
  * 그 객체가 이터러블 프로토콜을 구현한다, 또는 순회 가능하다고 한다. 
  */
-function makeIterator2(array){
+function makeIterator2(array) {
     var nextIndex = 0;
     return {
         next: () => {
-            return nextIndex < array.length 
-            ? {value: array[nextIndex++], done: false}
-            : {done: true};
+            return nextIndex < array.length
+                ? { value: array[nextIndex++], done: false }
+                : { done: true };
         }
     };
 }
 const iterableObj = {
-    [Symbol.iterator](){
-        return makeIterator2([1,2,3]);
+    [Symbol.iterator]() {
+        return makeIterator2([1, 2, 3]);
     }
 };
-for(const elem of iterableObj){
+for (const elem of iterableObj) {
     console.log(elem);
 }
 console.log(...iterableObj);
 ln();
 //Promise - 비동기 처리
-function getRandomPromise(){
+function getRandomPromise() {
     return new Promise((resolve, reject) => {
-        setTimeout(function(){
+        setTimeout(function () {
             const destiny = Math.random();
-            if (destiny > 0.5){
+            if (destiny > 0.5) {
                 resolve();
             } else {
                 reject();
@@ -212,21 +212,21 @@ function getRandomPromise(){
 ln();
 //[ES2017] Async / Await
 // Async(비동기)을 지원하는 환경에서 함수 선언시 일시적으로 Promise로 감싸짐
-async function returnTheAnswer(){
+async function returnTheAnswer() {
     return 42;
 }
 const implicitlyReturnedPromise = returnTheAnswer();
 console.log(implicitlyReturnedPromise instanceof Promise);  //true
 implicitlyReturnedPromise.then(answer => console.log(answer));  //42
 
-async function asyncExample(){
-    const a = setTimeout(()=>{42}, 3000);
+async function asyncExample() {
+    const a = setTimeout(() => { 42 }, 3000);
     //const a = await new Promise(resolve => {setTimeout(()=>resolve(42), 3000)});
     const b = await 42;
     let c;
     try {
-        c = await new Promise((_,reject)=>{reject("Error on await")});
-    } catch(e){
+        c = await new Promise((_, reject) => { reject("Error on await") });
+    } catch (e) {
         console.log(e);
     }
     console.log(`a: ${a}, b: ${b}, c: ${c}`);
